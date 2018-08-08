a06346ecc5120bc3792823c2e78847e3170d5758
kaidaguerre <kai@turbot.com>
2018-08-08 14:15:50 -0400

    Update finalize to console.log serialises turbot object and result

--- index.js
+++ index.js
@@ -7,8 +7,11 @@ const initialize = (event, context, callback) => {
 };
 
 const finalize = (event, context, init, err, result, callback) => {
-  console.log("TODO - publish Turbot commands:");
-  console.log(JSON.stringify(init.turbot.asProcessEvent(), null, 2));
+  const lamdbaResult = {
+    result,
+    turbot: init.turbot
+  };
+  console.log(JSON.stringify(lamdbaResult, null, 2));
   callback(err, result);
 };
 
